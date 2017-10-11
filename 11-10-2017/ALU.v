90df867d8877218d3859536a2e84a1eda6ddf227
Progyan Bhattacharya <progyanb@acm.org>
2017-10-11 21:28:25 +0530

    Lab: 11-10-2017: Completed ALU model

--- 11-10-2017/ALU.v
+++ 11-10-2017/ALU.v
@@ -22,10 +22,10 @@ endmodule // FullAdder
 
 // 4-bit Adder Module
 module Adder (
-    output reg [3:0] c,
-    output reg       carry,
-    input      [3:0] a, b,
-    input            sub);
+    output [3:0] c,
+    output       carry,
+    input  [3:0] a, b,
+    input        sub);
 
     wire w1, w2, w3;
     FullAdder fadd1(c[0], w1, a[0], b[0], 1'b0, sub);
@@ -34,6 +34,73 @@ module Adder (
     FullAdder fadd4(c[3], carry, a[3], b[3], w3, sub);
 endmodule // Adder
 
+// 4-bit And Module
+module myand (
+    output [3:0] c,
+    input  [3:0] a, b);
+
+    and(c[0], a[0], b[0]);
+    and(c[1], a[1], b[1]);
+    and(c[2], a[2], b[2]);
+    and(c[3], a[3], b[3]);
+endmodule // myand
+
+// 4-bit Or Module
+module myor (
+    output [3:0] c,
+    input  [3:0] a, b);
+
+    or(c[0], a[0], b[0]);
+    or(c[1], a[1], b[1]);
+    or(c[2], a[2], b[2]);
+    or(c[3], a[3], b[3]);
+endmodule // myor
+
+// 4-bit Not Module
+module mynot (
+    output [3:0] c,
+    input  [3:0] a);
+
+    not(c[0], a[0]);
+    not(c[1], a[1]);
+    not(c[2], a[2]);
+    not(c[3], a[3]);
+endmodule // mynot
+
+
+// 4-bit Xor Module
+module myxor (
+    output [3:0] c,
+    input  [3:0] a, b);
+
+    xor(c[0], a[0], b[0]);
+    xor(c[1], a[1], b[1]);
+    xor(c[2], a[2], b[2]);
+    xor(c[3], a[3], b[3]);
+endmodule // myxor
+
+// 4-bit Nand Module
+module mynand (
+    output [3:0] c,
+    input  [3:0] a, b);
+
+    nand(c[0], a[0], b[0]);
+    nand(c[1], a[1], b[1]);
+    nand(c[2], a[2], b[2]);
+    nand(c[3], a[3], b[3]);
+endmodule // mynand
+
+// 4-bit Nor Module
+module mynor (
+    output [3:0] c,
+    input  [3:0] a, b);
+
+    nor(c[0], a[0], b[0]);
+    nor(c[1], a[1], b[1]);
+    nor(c[2], a[2], b[2]);
+    nor(c[3], a[3], b[3]);
+endmodule // mynor
+
 // 1-bit 8x1 Multiplexer Module
 module Mux0 (
     output       out,
@@ -41,8 +108,8 @@ module Mux0 (
     input  [2:0] s);
 
     assign out = (
-            (~s[0] & ~s[1] & ~s[2] & d1) | (s[0] & ~s[1] & ~s[2] & d2) | (~s[0] & s[1] & ~s[2] & d3) | (s[0] & s[1] & ~s[2] & d4) |
-            (~s[0] & ~s[1] & s[2] & d5) | (s[0] & ~s[1] & s[2] & d6) | (~s[0] & s[1] & s[2] & d7) | (s[0] & s[1] & s[2] & d8));
+        (~s[0] & ~s[1] & ~s[2] & d1) | (s[0] & ~s[1] & ~s[2] & d2) | (~s[0] & s[1] & ~s[2] & d3) | (s[0] & s[1] & ~s[2] & d4) |
+        (~s[0] & ~s[1] & s[2] & d5) | (s[0] & ~s[1] & s[2] & d6) | (~s[0] & s[1] & s[2] & d7) | (s[0] & s[1] & s[2] & d8));
 endmodule // Mux0
 
 // 4-bit 8x1 Multiplexer Module
@@ -57,21 +124,54 @@ module Mux (
     Mux0 mux3(out[3], d1[3], d2[3], d3[3], d4[3], d5[3], d6[3], d7[3], d8[3], s);
 endmodule // Mux
 
+// Arithmetic and Logic Unit Module
 module ALU (
-    output reg [3:0] c,
-    output reg       carry,
-    input      [2:0] op,
-    input      [3:0] a, b);
+    output [3:0] c,
+    output       carry,
+    input  [2:0] op,
+    input  [3:0] a, b);
 
     wire [3:0] w1, w2, w3, w4, w5, w6, w7, w8;
-    wire wc1, wc2;
-    Adder add(w1, wc1, a, b, 1'b0);
-    Adder sub(w2, wc2, a, b, 1'b1);
-    and(w3, a, b);
-    or(w4, a, b);
-    not(w5, a);
-    xor(w6, a, b);
-    nand(w7, a, b);
-    nor(w8, a, b);
-    Mux mux(c, w1, w2, w3, w4, w5, w6, w7, w8, op);
-endmodule
+    wire       wc1, wc2;
+    Adder  add(w1, wc1, a, b, 1'b0);
+    Adder  sub(w2, wc2, a, b, 1'b1);
+    myand  mand(w3, a, b);
+    myor   mor(w4, a, b);
+    mynot  mnot(w5, a);
+    myxor  mxor(w6, a, b);
+    mynand mnand(w7, a, b);
+    mynor  mnor(w8, a, b);
+    Mux    mux(c, w1, w2, w3, w4, w5, w6, w7, w8, op);
+    assign carry = (~op[0] & w1) | (op[0] & w2);
+endmodule // ALU
+
+// Test Genrator for ALU
+module TestGen (
+    input      [3:0] c,
+    input            carry,
+    output reg [2:0] op,
+    output reg [3:0] a, b);
+
+    // wire w1;
+    // assign w1 = { op, a, b };
+    initial begin
+        $monitor($time,,, "\nOpcode: %b  A: %b  B: %b\nOutput: %b  Carry: %b\n", op, a, b, c, carry);
+            { op, a, b } = { 3'd0, 4'd8, 4'd4 };
+        #02 op = 3'd1;
+        #02 op = 3'd2;
+        #02 op = 3'd3;
+        #02 op = 3'd4;
+        #02 op = 3'd5;
+        #02 op = 3'd6;
+        #02 op = 3'd7;
+    end
+endmodule // TestGen
+
+// WorkBench Module
+module wb;
+    wire [3:0] a, b, c;
+    wire [2:0] op;
+    wire       carry;
+    ALU myalu(c, carry, op, a, b);
+    TestGen test(c, carry, op, a, b);
+endmodule // wb
